ac3dbc9127d24878beb391565e705a6167bd99cb
Joe-Mwangi <joem99271@gmail.com>
2023-03-03 15:47:26 +0300

    added transaction history

--- Program.cs
+++ Program.cs
@@ -7,34 +7,34 @@ var account4 = new BankAccount("Kim", 3000);
 var account5 = new BankAccount("Bob", 1500);
 
 account5.MakeDeposit(700, DateTime.Now, "Mshwari saving");
-Console.WriteLine($"Account: {account5.Number}\nOwner: {account5.Owner}\nBalance: {account5.Balance}\n");
+Console.WriteLine(account5.GetAccountHistory());
 
 account.MakeWithdrawal(900, DateTime.Now, "Sherehe");
-Console.WriteLine($"Account: {account.Number}\nOwner: {account.Owner}\nBalance: {account.Balance}\n");
+Console.WriteLine(account.GetAccountHistory());
 
 account5.MakeWithdrawal(2000, DateTime.Now, "Pay loan");
-Console.WriteLine($"Account: {account5.Number}\nOwner: {account5.Owner}\nBalance: {account5.Balance}\n");
+Console.WriteLine(account5.GetAccountHistory());
 
-// Test for a negative balance.
-try
-{
-    account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
-}
-catch (InvalidOperationException e)
-{
-    Console.WriteLine("Exception caught trying to overdraw");
-    Console.WriteLine(e.ToString());
-}
+// // Test for a negative balance.
+// try
+// {
+//     account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+// }
+// catch (InvalidOperationException e)
+// {
+//     Console.WriteLine("Exception caught trying to overdraw");
+//     Console.WriteLine(e.ToString());
+// }
 
-// Test that the initial balances must be positive.
-BankAccount invalidAccount;
-try
-{
-    invalidAccount = new BankAccount("invalid", -55);
-}
-catch (ArgumentOutOfRangeException e)
-{
-    Console.WriteLine("Exception caught creating account with negative balance");
-    Console.WriteLine(e.ToString());
-    return;
-}
+// // Test that the initial balances must be positive.
+// BankAccount invalidAccount;
+// try
+// {
+//     invalidAccount = new BankAccount("invalid", -55);
+// }
+// catch (ArgumentOutOfRangeException e)
+// {
+//     Console.WriteLine("Exception caught creating account with negative balance");
+//     Console.WriteLine(e.ToString());
+//     return;
+// }
