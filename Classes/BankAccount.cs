895d8b6853e64a4acb4b12b992a0b0502d6a07aa
Joe-Mwangi <joem99271@gmail.com>
2023-03-03 13:51:57 +0300

    added Transaction

--- Classes/BankAccount.cs
+++ Classes/BankAccount.cs
@@ -5,7 +5,16 @@ public class BankAccount
     private static int accountNumberSeed = 1000000000;
     public string Number { get; }
     public string Owner { get; set; }
-    public decimal Balance { get; }
+    public decimal Balance 
+    {
+        get {
+            decimal balance = 0;
+            foreach(var item in allTransactions) {
+                balance += item.Amount;
+            }
+            return balance;
+        }
+    }
 
     public BankAccount(string name, decimal initialBalance) 
     {
@@ -15,11 +24,26 @@ public class BankAccount
         accountNumberSeed++;
     }
 
+    private List<Transaction> allTransactions = new List<Transaction>();
+
     public void MakeDeposit(decimal amount, DateTime date, string note)
     {
+        if(amount <= 0) {
+            throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+        }
+        var deposit = new Transaction(amount, date, note);
+        allTransactions.Add(deposit);
     }
 
     public void MakeWithdrawal(decimal amount, DateTime date, string note)
     {
+        if(amount <= 0) {
+            throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+        }
+        if(Balance  - amount < 0) {
+            throw new InvalidOperationException(nameof(amount), "Not sufficient funds in your account");
+        }
+        var withdrawal = new Transaction(-amount, date, note);
+        allTransactions.Add(withdrawal);
     }
 }
