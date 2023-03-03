dcd749af5d434d34d3456ea19846763f55a90375
Joe-Mwangi <joem99271@gmail.com>
2023-03-03 13:11:52 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using Classes;
+
+var account = new BankAccount("Joe", 1000);
+var account2 = new BankAccount("Mwangi", 2000);
+var account3 = new BankAccount("John", 500);
+var account4 = new BankAccount("Kim", 3000);
+var account5 = new BankAccount("Bob", 1500);
+Console.WriteLine($"Account: {account5.Number}\nOwner: {account5.Owner}\nBalane: {account5.Balance}");
